077a58a48eb170887ad8c8a13d7e6165f8bb0fe2
skullhan0727 <menghan727@gmail.com>
2019-08-30 16:42:21 +0800

    opencv ok

--- insumon_ocr/app/src/main/java/com/example/insumon_ocr/MainActivity.java
+++ insumon_ocr/app/src/main/java/com/example/insumon_ocr/MainActivity.java
@@ -25,15 +25,12 @@ import static org.bytedeco.javacpp.opencv_imgproc.cvtColor;
 
 public class MainActivity extends AppCompatActivity {
     public opencv_core.Mat roi;
-    public opencv_core.Mat blurred;
     public ImageView imageView;
 
-    public SeekBar skb;
 
-    public opencv_core.Mat mat;
 
     Bitmap bitmap;
-    Bitmap bitmap1;
+    Bitmap bitmap_after;
     boolean clicked = false;
 
     int b = 1351;
@@ -49,14 +46,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+
         imageView = (ImageView) findViewById(R.id.imgV);
-        //skb = (SeekBar) findViewById(R.id.seekBar);
+
 
         imageView.invalidate();
         BitmapDrawable drawable = (BitmapDrawable) imageView.getDrawable();
         bitmap = drawable.getBitmap();
 
-        //opencv_core.IplImage iplImage = this.bitmapToIplImage(bitmap);
+
+
 
         converterToBitmap = new AndroidFrameConverter();
         converterToIplImage = new OpenCVFrameConverter.ToIplImage();
@@ -66,18 +65,19 @@ public class MainActivity extends AppCompatActivity {
         Frame frame = converterToBitmap.convert(bitmap);
 
         roi = converterToIplImage.convertToMat(frame);
-        cvtColor(roi, roi, opencv_imgproc.COLOR_RGB2GRAY);
+
 
         process(b,c);
     }
 
     public void process(int bsize,int c){
-        //roi = opencv_imgcodecs.imread("C:/blood/demo_output/279_roi.jpg", CV_8UC1);
 
-
+        //gray
+        opencv_core.Mat gray=new opencv_core.Mat(roi.rows(),roi.cols(),roi.type());
+        cvtColor(roi, gray, opencv_imgproc.COLOR_RGB2GRAY);
         //Gaussianblur
-        blurred=new opencv_core.Mat(roi.rows(),roi.cols(),roi.type());
-        opencv_imgproc.GaussianBlur(roi, blurred,new opencv_core.Size(15,15),0,0,0);
+        opencv_core.Mat blurred=new opencv_core.Mat(roi.rows(),roi.cols(),roi.type());
+        opencv_imgproc.GaussianBlur(gray, blurred,new opencv_core.Size(15,15),0,0,0);
         //thresh
         opencv_core.Mat thresh= new opencv_core.Mat(roi.rows(), roi.cols(), roi.type());
         opencv_imgproc.adaptiveThreshold(blurred, thresh, 255, 1,0, bsize, c);
@@ -88,7 +88,7 @@ public class MainActivity extends AppCompatActivity {
         for (int i=0;i<20;i++) {
             opencv_imgproc.erode( erode1, erode1, element_e1);
         }
-//		BufferedImage image = matToBufferedImage(erode1);
+
 
         //dialte1  iter:2
         opencv_core.Mat dialte1=erode1;
@@ -98,47 +98,25 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
-        Frame frame1 = converterToMat.convert(dialte1);
-        bitmap1 = converterToBitmap.convert(frame1);
-//
-//        imageView.setImageBitmap(bitmap1);
 
 
-        imageView.setImageBitmap(bitmap1);
+
+
+        Frame frame_after = converterToMat.convert(dialte1);
+        bitmap_after = converterToBitmap.convert(frame_after);
+
+        imageView.setImageBitmap(bitmap_after);
 
 
     }
 
 
-    public Bitmap IplImageToBitmap(opencv_core.IplImage iplImage) {
-        Bitmap bitmap = null;
-        bitmap = Bitmap.createBitmap(iplImage.width(), iplImage.height(),
-                Bitmap.Config.ARGB_8888);
-        bitmap.copyPixelsFromBuffer(iplImage.getByteBuffer());
-        return bitmap;
-    }
-
-
-    public opencv_core.IplImage bitmapToIplImage(Bitmap bitmap) {
-        opencv_core.IplImage iplImage;
-        iplImage = opencv_core.IplImage.create(bitmap.getWidth(), bitmap.getHeight(),
-                IPL_DEPTH_8U, 4);
-        bitmap.copyPixelsToBuffer(iplImage.getByteBuffer());
-        return iplImage;
-    }
 
 
     public void trans(View view) {
         b = b+50;
         process(b,c);
         System.out.println(String.valueOf(b));
-//        if(!clicked){
-//            process(515);
-//            clicked = !clicked;
-//        }else{
-//            imageView.setImageBitmap(bitmap);
-//            clicked = !clicked;
-//        }
     }
 
     public void transc(View view) {
